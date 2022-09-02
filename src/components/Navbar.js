475a3c4c8f4a8e4e1370994cd3ffebd9f6f64817
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-09-02 15:20:36 +0300

    NavBar, Button, Dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import '../styles/Navbar.css'
+import { Link } from 'react-router-dom'
+import { GiBonsaiTree } from 'react-icons/gi'
+import { navItems } from '../items/NavItems'
+import Button from './Button'
+import Dropdown from './Dropdown'
+import { useState } from 'react'
+
 
 const Navbar = () => {
-    return (
-        <div>
+    const [dropdown, setDropdown] = useState(false);
 
-        </div>
+    return (
+        <>
+            <nav className='navbar'>
+                <Link to="/" className='navbar-logo'>
+                    NATURE <GiBonsaiTree />
+                </Link>
+                <ul className='nav-items'>
+                    {navItems.map((item) => {
+                        if (item.title === 'Winter') {
+                            return (
+                                <li key={item.id} className={item.cName}
+                                    onMouseEnter={() => setDropdown(true)}
+                                    onMouseLeave={() => setDropdown(false)}
+                                >
+                                    <Link to={item.path}>
+                                        {item.title}</Link>
+                                    {dropdown && <Dropdown />}
+                                </li>)
+                        }
+                        return (
+                            <li key={item.id} className={item.cName}>
+                                <Link to={item.path}>{item.title}</Link>
+                            </li>
+                        )
+                    }
+                    )}
+                </ul>
+                <Button />
+            </nav>
+        </>
     )
 }
 
